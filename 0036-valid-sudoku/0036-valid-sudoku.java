1d2a79a389c6467a2c521d90134ff19e03ce5ff3
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-09-11 00:23:21 -0700

    Time: 3 ms (57.07%), Space: 44.4 MB (60.82%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,28 +1,33 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        
+        // hashset to check duplicates for each row, col, box
         HashSet<Character>[] rows = new HashSet[9];
         HashSet<Character>[] cols = new HashSet[9];
         HashSet<Character>[] boxes = new HashSet[9];
         
+        //initiate empty hashSets to each index
         for(int i = 0; i < 9; i++){
             rows[i] = new HashSet<>();
             cols[i] = new HashSet<>();
             boxes[i] = new HashSet<>();
         }
         
+        //check each sudoku cells
         for (int r = 0; r < board.length; r ++){
             for (int c = 0; c < board[0].length; c++){
                 if (board[r][c] == '.'){
                     continue;
                 }else{
                     char value = board[r][c];
+                    // i.e) if row = 1 and col = 3 => box = 1
                     int boxNum = (r/3) * 3 + (c/3);
                     
+                    //check for duplicates in all hashSet 
                     if(rows[r].contains(value) || cols[c].contains(value) || boxes[boxNum].contains(value)){
                         return false;
                     }
                     
+                    //if no duplicates add the value to the hashSet
                     rows[r].add(value);
                     cols[c].add(value);
                     boxes[boxNum].add(value);
