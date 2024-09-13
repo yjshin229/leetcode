a7efdb3ae67032f7f78ac49ac4547463285ef51a
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-09-12 23:22:53 -0700

    Time: 1 ms (98.07%), Space: 44.8 MB (76.30%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,41 +1,24 @@
 class Solution {
     public int jump(int[] nums) {
-//         int res = 0;
+        int res = 0;
         
-//         int jump = 0, range = 0;
+        int jump = 0, range = 0;
 
-//         if(nums.length == 1){
-//             return 0;
-//         }
+        if(nums.length == 1){
+            return 0;
+        }
         
-//         for (int i = 0; i < nums.length; i++){
-//             jump = Math.max(nums[i]+i, jump);
-
-//             if(i == nums.length -1){
-//                     return res;
-//                 }else if(i == range){
-//                 res ++;
-//                 range += jump;
-                
-//             }
-//         }    
-//         return res;
-          int n=nums.length;
-        if(n==1) return 0;
-        int count=0;
-        int far=0,range=0;
-        for(int i=0;i<n;i++){
-            far=Math.max(nums[i]+i,far);
-            if(i==range){
-                count++;
-                range=far;
-                if(range>=n-1){
-                    return count;
+        for (int i = 0; i < nums.length; i++){
+            jump = Math.max(nums[i]+i, jump);
+            if(i == range){
+                res ++;
+                range = jump;
+                if(range >= nums.length -1){
+                    return res;
                 }
             }
-           
-        }
-        return count;
+        }    
+        return res;
 
     }
     
