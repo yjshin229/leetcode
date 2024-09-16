25e84e4137066b65cd92cac743b4a9924df5c164
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-09-15 18:11:19 -0700

    Time: 108 ms (21.78%), Space: 59.3 MB (43.57%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -5,23 +5,21 @@
 var merge = function(intervals) {
     
     intervals.sort((a, b) => (a[0] - b[0]));
-    let start = 0
-    let end = intervals.length-1;
-    while(start < intervals.length-1){
-        if(intervals[start][1] >= intervals[start+1][0]){
+    let idx = 0
+    while(idx < intervals.length-1){
+        if(intervals[idx][1] >= intervals[idx+1][0]){
             let temp = []
-            if(intervals[start][1] > intervals[start+1][1]){
-                temp = [intervals[start][0], intervals[start][1]]
+            if(intervals[idx][1] > intervals[idx+1][1]){
+                temp = [intervals[idx][0], intervals[idx][1]]
 
             }else{
-                temp = [intervals[start][0], intervals[start+1][1]]
+                temp = [intervals[idx][0], intervals[idx+1][1]]
 
             }
-            intervals.splice(start,2)
-            intervals.splice(start,0,temp)
-            end --;
+            intervals.splice(idx,2)
+            intervals.splice(idx,0,temp)
         }else{
-            start ++;
+            idx ++;
         }
     }
     return intervals
