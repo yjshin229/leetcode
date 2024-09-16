9b032e1352c70950f042387085fb6a32c5a000ed
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-09-15 19:03:02 -0700

    Time: 81 ms (26.18%), Space: 54.6 MB (32.96%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.js
+++ 0057-insert-interval/0057-insert-interval.js
@@ -16,12 +16,13 @@ var insert = function(intervals, newInterval) {
 
         
         if(intervals[idx][1] >= intervals[idx+1][0]){
-            let temp =[]
-            if(intervals[idx][1] > intervals[idx+1][1]){
-                temp = [intervals[idx][0], intervals[idx][1]]
-            }else{
-                temp = [intervals[idx][0], intervals[idx+1][1]]
-            }
+            
+            let temp =[Math.min(intervals[idx][0] , intervals[idx+1][0]), Math.max(intervals[idx][1] , intervals[idx+1][1])]
+            // if(intervals[idx][1] > intervals[idx+1][1]){
+            //     temp = [intervals[idx][0], intervals[idx][1]]
+            // }else{
+            //     temp = [intervals[idx][0], intervals[idx+1][1]]
+            // }
             intervals.splice(idx,2)
             intervals.splice(idx, 0, temp)
         }else{
