470ce2232f3371d889b0069f676e5604cc45df4a
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-09-15 18:10:15 -0700

    Time: 131 ms (7.78%), Space: 60.3 MB (23.45%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    
+    intervals.sort((a, b) => (a[0] - b[0]));
+    console.log(intervals)
+    let start = 0
+    let end = intervals.length-1;
+    while(start < end){
+        if(intervals[start][1] >= intervals[start+1][0]){
+            let temp = []
+            if(intervals[start][1] > intervals[start+1][1]){
+                temp = [intervals[start][0], intervals[start][1]]
+
+            }else{
+                temp = [intervals[start][0], intervals[start+1][1]]
+
+            }
+            intervals.splice(start,2)
+            intervals.splice(start,0,temp)
+            end --;
+        }else{
+            start ++;
+        }
+    }
+    return intervals
+};
