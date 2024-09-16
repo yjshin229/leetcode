719828886ff5800785109ff0d5a74f67286fe730
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-09-15 20:08:29 -0700

    Time: 50 ms (68.47%), Space: 48.6 MB (91.36%) - LeetHub

---
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.js
@@ -0,0 +1,76 @@
+/**
+ * @param {number} n
+ * @return {number[][]}
+ */
+var generateMatrix = function(n) {
+    let res = new Array(n)
+    .fill().map(() => new Array(n).fill(0));
+    
+    let count = 1
+    let r = 0
+    let c = 0
+    
+    let moveX = 1
+    let moveY = 0;
+    
+    const changeMoveDirection = () => {
+        let temp = moveX
+        moveX = -1 * moveY
+        moveY = temp
+    }
+    while(count <= n*n){
+        //move right
+        if(moveX === 1 && moveY === 0){
+            if(res[r][c] === 0){
+                res[r][c] = count
+                count ++
+            }
+           
+            if(c === n-1 || res[r][c+1] !== 0){
+                changeMoveDirection();
+            }else{
+               c++; 
+            }
+            
+        // move down    
+        }else if(moveX === 0 && moveY === 1){
+            if(res[r][c] === 0){
+                res[r][c] = count
+                count ++
+            }
+           
+            if( r === n -1 || res[r+1][c] !== 0){
+                changeMoveDirection();
+            }else{
+                r++; 
+            }
+        //move left   
+        }else if(moveX === -1 && moveY === 0){
+            if(res[r][c] === 0){
+                res[r][c] = count
+                count ++
+            }
+           
+            if( c === 0 || res[r][c-1] !== 0){
+                changeMoveDirection();
+            }else{
+                c --; 
+            }
+            
+        //move up
+        }else{
+            if(res[r][c] === 0){
+                res[r][c] = count
+                count ++
+            }
+           
+            if( r === 0 || res[r-1][c] !== 0){
+                changeMoveDirection();
+            }else{
+                r --; 
+            }
+        }
+            
+    }
+    return res
+};
