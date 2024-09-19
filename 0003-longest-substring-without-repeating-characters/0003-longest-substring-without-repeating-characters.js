eaa7d24a6153362bfe1c91920c4c9148e061879a
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-09-18 20:07:51 -0700

    Time: 189 ms (17.83%), Space: 57.5 MB (15.01%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let temp = new Set()
+    let max = 0
+    let left = 0
+    let right = 0
+    const sLen = s.length 
+    while(left <= right && right <= sLen-1){
+        temp.add(s[right]);
+        let tempSize = temp.size
+        
+        if(tempSize < right - left + 1){
+            left ++;
+            let currentStr = s.slice(left,right+1)
+            temp.clear()
+            Array.from(currentStr).forEach((c) => {
+                temp.add(c)
+            })
+            right ++;
+        }else{
+            max = Math.max(right-left +1, max)
+            right ++
+        }
+        
+    }
+    return max
+};
