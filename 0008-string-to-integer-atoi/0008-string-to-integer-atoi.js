98dd43718c4ffb364da7bddeb7dea310d0c84616
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-09-24 18:38:44 -0700

    Time: 93 ms (15.18%), Space: 53.7 MB (43.01%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
@@ -15,18 +15,15 @@ var myAtoi = function(s) {
                 continue
             }
         }else if(Number.isNaN(parseInt(trimmed[i]))){
-            console.log(parseInt(trimmed[i]), i)
             break;
         }else{
             res += trimmed[i]
-            console.log(res)
         }
     }
     
     if(res === '') return 0
     
     if(negative){
-        console.log('neg')
         if(parseInt(res) * -1 < Math.pow(-2, 31)){
             return Math.pow(-2, 31)
         }else{
