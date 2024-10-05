ecf11c451e97990ca87fb159a6b83b1284e0a80f
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-10-05 13:26:42 +0000

    Time: 75 ms (50.39%), Space: 53.2 MB (28%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -3,7 +3,6 @@
  * @return {number}
  */
 var reverse = function(x) {
-    console.log(Math.pow(2,31))
     if(x < Math.pow(-2, 31) || x > Math.pow(2,31) -1) return 0
     let xStr = x.toString()
     let res = ""
