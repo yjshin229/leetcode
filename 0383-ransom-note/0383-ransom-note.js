2aaea8e3fb3cba6837c62340ec0cb4384d1601c7
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-10-10 02:46:21 -0700

    Time: 122 ms (5.19%), Space: 58.2 MB (5.39%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.js
+++ 0383-ransom-note/0383-ransom-note.js
@@ -6,12 +6,11 @@
 var canConstruct = function(ransomNote, magazine) {
     if(ransomNote.length > magazine.length) return false
     
-    let magazineArr = magazine.split("")
     for(const char of ransomNote){
-        const idx = magazineArr.indexOf(char)
+        const idx = magazine.indexOf(char)
         console.log(idx)
         if(idx === -1) return false
-        else magazineArr.splice(idx,1)
+        else magazine = magazine.substring(0,idx) + magazine.substring(idx+1)
     }
     return true
 };
