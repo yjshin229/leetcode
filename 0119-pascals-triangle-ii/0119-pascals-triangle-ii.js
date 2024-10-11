6aa2e64cbcee3960085666a623842272bbb801c5
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-10-11 16:46:36 -0700

    Time: 48 ms (80.97%), Space: 48.4 MB (95.08%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function(rowIndex) {
+//     if(rowIndex === 0 ) return [1]
+//     if(rowIndex === 1) return [1,1]
+    
+//     let res = []
+//     for(let i =0; i < rowIndex; i++){
+//         if(i === 0 || i === rowIndex - 1){
+//             res.psuh(1)
+//         }else{
+            
+//         }
+//     }
+    
+    let res = []
+    for(let i = 0; i <= rowIndex; i++){
+        if(i === 0){
+            res.push([1])
+        }else if (i === 1){
+            res.push([1,1])
+        }else{
+            let temp = []
+            for(let j = 0; j <= res[i-1].length; j++){
+                if(j === 0 || j === res[i-1].length){
+                    temp.push(1)
+                }else{
+                    temp.push(res[i-1][j-1] + res[i-1][j])
+                }
+            }
+            res.push(temp)
+        }
+    }
+    return res[res.length-1]
+};
