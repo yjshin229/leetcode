293acfb96278e38cf137ada1c2386f3939a6a92e
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-10-11 17:07:34 -0700

    Time: 541 ms (7.11%), Space: 59.8 MB (5.02%) - LeetHub

---
+++ 0136-single-number/0136-single-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    if(nums.length === 1) return nums[0]
+    
+    let seen = new Set()
+    for (let i = 0; i < nums.length; i ++){
+        const curr = nums[i]
+        
+        nums[i] = NaN
+        console.log(seen.has(curr))
+        if(!nums.includes(curr) && !seen.has(curr)) return curr
+        seen.add(curr)
+    }
+};
