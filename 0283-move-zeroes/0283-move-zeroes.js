7291a6d4bccd4775f673f7706afe7f07bda390d7
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-10-15 01:50:31 -0700

    Time: 120 ms (9.53%), Space: 54.8 MB (27.07%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    if(nums.length === 1) return
+
+    let left = 0
+    let right = left
+
+    const swap = (idx1, idx2) => {
+        const temp = nums[idx1]
+        nums[idx1] = nums[idx2]
+        nums[idx2] = temp
+    }
+
+    while(right < nums.length){
+        if(nums[right] === 0){
+            right ++;
+            continue;
+        }
+        if(nums[left] === 0){
+            swap(left, right);
+            left ++;
+            right = left
+            continue
+        }
+        left ++;
+        right ++;
+    }
+};
