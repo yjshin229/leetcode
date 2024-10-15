5384b69a0b508de84a2e259f39aaf449d7dbfe05
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-10-15 02:35:24 -0700

    Time: 102 ms (22.29%), Space: 57.7 MB (30.18%) - LeetHub

--- 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.js
+++ 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.js
@@ -4,7 +4,7 @@
 var MovingAverage = function(size) {
     this.size = size
     this.arr = [];
-
+    this.sum = 0
 };
 
 /** 
@@ -12,19 +12,26 @@ var MovingAverage = function(size) {
  * @return {number}
  */
 MovingAverage.prototype.next = function(val) {
-    this.arr.push(val)
+    // this.arr.push(val)
     
-    let sum = 0
-    let count = 0
-    for(let i = this.arr.length -1 ; i >=0; i--){
-        if(count === this.size) break
+    // let sum = 0
+    // let count = 0
+    // for(let i = this.arr.length -1 ; i >=0; i--){
+    //     if(count === this.size) break
         
-        count ++
-        sum += this.arr[i]
+    //     count ++
+    //     sum += this.arr[i]
     
+    // }
+    // console.log(sum)
+    // return sum / count
+
+    this.arr.push(val);
+    this.sum += val;
+    if (this.arr.length > this.size) {
+        this.sum -= this.arr[this.arr.length - this.size - 1]
     }
-    console.log(sum)
-    return sum / count
+    return this.sum / Math.min(this.arr.length, this.size);
 };
 
 /** 
