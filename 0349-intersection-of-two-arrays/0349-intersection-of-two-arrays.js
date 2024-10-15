48828f98f98ac76687132954b1199ea80f904860
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-10-15 02:42:09 -0700

    Time: 61 ms (30.14%), Space: 52.1 MB (5.12%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    console.log(nums1.concat(nums2))
+    let intersection = new Set(nums1.concat(nums2))
+    let intersectionArr = [...intersection]
+
+    let res = []
+    for(const num of intersectionArr ){
+        if(nums1.includes(num) && nums2.includes(num)){
+            res.push(num)
+        }
+    }
+    return res
+};
