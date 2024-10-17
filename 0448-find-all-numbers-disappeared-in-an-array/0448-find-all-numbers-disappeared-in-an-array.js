8195af606b5028d3a7d397f1ed91137e916a68bf
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-10-17 01:22:01 -0700

    Time: 158 ms (5.06%), Space: 60.1 MB (63.27%) - LeetHub

---
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function(nums) {
+    const n = nums.length
+    let res = []
+    nums.sort()
+     for (let i = 0; i < n; i++) {
+        let index = Math.abs(nums[i]) - 1; // Get the index for the current number
+        // Mark the number at this index as negative (if it's not already)
+        if (nums[index] > 0) {
+            nums[index] = -nums[index];
+        }
+    }
+
+    // Collect all the indices where the value is positive (i.e., missing numbers)
+    for (let i = 0; i < n; i++) {
+        if (nums[i] > 0) {
+            res.push(i + 1); // These are the missing numbers
+        }
+    }
+    return res
+};
