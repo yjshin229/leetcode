66999d9df6d892f105f103ae9cf44f5a61fd60ce
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-10-17 14:23:40 -0700

    Time: 27 ms (100%), Space: 55.9 MB (68.88%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function(g, s) {
+
+    g.sort((a,b) => a-b)
+    s.sort((a,b) => a-b)
+    let child = 0;
+    let cookie = 0;
+    
+    // Use two pointers to traverse both arrays
+    while (child < g.length && cookie < s.length) {
+        // If the current cookie can satisfy the current child
+        if (s[cookie] >= g[child]) {
+            child++; // Move to the next child
+        }
+        cookie++; // Move to the next cookie
+    }
+    
+    return child;
+};
