4f123e9dfbce83e7fd28f3217353c65cbae183fb
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-10-18 00:52:27 -0700

    Time: 12 ms (100%), Space: 59.6 MB (6.13%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    if(n === 0 ) return false
+    while(n !== 1){
+        if(n % 3 === 0){
+            n /= 3
+        }else{
+            return false
+        }
+    }
+
+    return true
+};
