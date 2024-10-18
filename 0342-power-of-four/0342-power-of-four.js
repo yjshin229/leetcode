57aff4e423d3710559960977665a1e5a37590e54
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-10-18 00:57:32 -0700

    Time: 0 ms (100%), Space: 52.4 MB (49.6%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.js
+++ 0342-power-of-four/0342-power-of-four.js
@@ -4,24 +4,16 @@
  */
 var isPowerOfFour = function(n) {
     if(n === 0) return false
-    let isNegative = false
     if(n < 0){
-        // isNegative = true
         return false
     }
     n = Math.abs(n)
-    let count = 0
     while(n !== 1){
         if(n % 4 === 0){
             n /= 4
-            count ++;
         }else{
             return false
         }
     }
-
-    // if(isNegative === true && count % 2 === 1){
-    //     return false
-    // }
     return true
 };
