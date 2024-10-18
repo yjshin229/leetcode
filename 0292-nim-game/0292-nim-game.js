75e232b055449f111b963d75413b808d644632eb
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-10-18 00:47:40 -0700

    Time: 0 ms (100%), Space: 48.6 MB (73.86%) - LeetHub

---
+++ 0292-nim-game/0292-nim-game.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var canWinNim = function(n) {
+
+    const backtrack = (remain) => {
+
+    }
+    if(n <=3){
+        return true
+    }else{
+        if(n % 4 === 0){
+            return false
+        }
+        return true
+    }
+
+
+};
