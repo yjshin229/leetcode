2328b43387b7c82fd8ee6d148cfee45c93c39c03
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-10-18 01:03:51 -0700

    Time: 7 ms (100%), Space: 50.3 MB (7.5%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.js
+++ 0367-valid-perfect-square/0367-valid-perfect-square.js
@@ -3,11 +3,21 @@
  * @return {boolean}
  */
 var isPerfectSquare = function(num) {
-    if(num === 1) return true
-    for(let i = 2; i <= 46340; i++){
-        if(i * i === num){
+    // if(num === 1) return true
+    // for(let i = 2; i <= 46340; i++){
+    //     if(i * i === num){
+    //         return true
+    //     }
+    // }
+    // return false
+    for(let i = 1; i < Math.pow(2,31) -1; i+= 2){
+        if(num === 0){
             return true
+        }else if(num < i){
+            return false
+        }else{
+            num -= i
         }
     }
-    return false
+    return true
 };
