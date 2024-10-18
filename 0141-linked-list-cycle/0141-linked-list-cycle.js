7500f0dac9dc22737d4f83ca364d436d2c4b179e
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-10-17 17:15:27 -0700

    Time: 63 ms (63.34%), Space: 52 MB (98.29%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -11,16 +11,8 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    let listMap = {}
     let BIG_NUMBER = Math.pow(10,5) + 1
 
-    // for(let current = head; !current; current = current.next){
-    //     if(!current.val === BIG_NUMBER) {
-    //         current.val = BIG_NUMBER 
-    //     }else{
-    //         return true
-    //     }
-    // }
     let current = head
     while(current){
         if(current.val !== BIG_NUMBER) {
