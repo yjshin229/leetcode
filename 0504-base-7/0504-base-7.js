8ced6cf7e68f0efce724077931e1a3ec974a3f74
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-10-18 21:12:58 -0700

    Time: 0 ms (100%), Space: 48.7 MB (68.2%) - LeetHub

--- 0504-base-7/0504-base-7.js
+++ 0504-base-7/0504-base-7.js
@@ -3,5 +3,17 @@
  * @return {string}
  */
 var convertToBase7 = function(num) {
-    return num.toString(7)
+    // return num.toString(7)
+
+    if (num === 0) return "0"
+
+    let temp = Math.abs(num)
+    let res = ""
+
+    while(temp > 0){
+        res = temp % 7 + res
+        temp = Math.floor(temp / 7)
+    }
+
+    return num > 0 ? res : `-${res}`
 };
