7689a1023690433590ee84432ed0817f1a9433de
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-10-18 20:30:28 -0700

    Time: 0 ms (100%), Space: 49 MB (17.78%) - LeetHub

--- 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.js
+++ 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.js
@@ -6,32 +6,16 @@ var toHex = function(num) {
     const hexMap = {0:'0', 1:'1', 2:'2', 3:'3', 4:'4', 5:'5', 6:'6', 7:'7', 8:'8', 9:'9', 10:'a', 11:'b', 12:"c", 13:"d", 14:"e", 15: "f"}
 
     if(num === 0) return "0"
-    const getDigit = () => {
-        let temp = 1
-        while(temp * 16 <= num){
-            temp *= 16
-            // count ++;
-        }
-        return temp
-    }
+
     let res = ""
 
     if(num < 0 ){
         num = 0xFFFFFFFF + num + 1;
     }
     while(num > 0){
-        // if(num < 16){
-        //     res += hexMap[num]
-        //     num = 0
-        // }else{
-        //     let temp = getDigit()
-        //     let digit = Math.floor(num / temp)
-        //     res += hexMap[digit]
-        //     num -= temp * digit
-        // }
-         let remainder = num % 16;  // Get the remainder (hex digit)
-        res = hexMap[remainder] + res;  // Add the hex digit to the result
-        num = Math.floor(num / 16);  // Move to the next higher digit
+        let remainder = num % 16;
+        res = hexMap[remainder] + res;
+        num = Math.floor(num / 16);
     } 
     
     
