f2e936a306ee0710721e1af4d5b896b1e89ec48e
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-10-20 21:43:16 -0700

    Time: 3 ms (98.85%), Space: 54.7 MB (69.23%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.js
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.js
@@ -19,15 +19,12 @@ var getMinimumDifference = function(root) {
     }
 
     dfs(root)
+    data.sort((a,b) => a-b)
     let difference = Math.pow(10, 6)
     if(data.length === 2) return Math.abs(data[0] - data[1])
-    for(let i = 0; i < data.length; i++){
-        if(data[i] === null) continue
-        for(let j = i + 1; j < data.length; j++){
-            if(data[j] === null) continue
-            let temp = Math.abs(data[i] - data[j])
-            if(temp < difference) difference = temp
-        }
+    for(let i = 1; i < data.length; i++){
+        let temp = Math.abs(data[i-1] - data[i])
+        if(temp < difference) difference = temp
     }
 
     return difference
