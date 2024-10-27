30bc236407c13cb427bd264f65b2f61128878b32
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-10-26 21:14:41 -0700

    Time: 3 ms (33.33%), Space: 52.7 MB (11.11%) - LeetHub

--- 0598-range-addition-ii/0598-range-addition-ii.js
+++ 0598-range-addition-ii/0598-range-addition-ii.js
@@ -1,47 +1,3 @@
-// /**
-//  * @param {number} m
-//  * @param {number} n
-//  * @param {number[][]} ops
-//  * @return {number}
-//  */
-// var maxCount = function(m, n, ops) {
-//     if(!ops.length) return  m * n
-//     if(ops.length === 1) return ops[0][0] * ops[0][1]
-//     let grid = Array.from({length :  m}, () => Array.from({length : n}).fill(0))
-
-//     let map = {}
-
-//     let max = 0
-//     let count = 0
-//     const addToMap = (row,col) => {
-//         for(let r = 0; r <row; r++){
-//             for(let c = 0; c<col; c++){
-//                 map[`${r},${c}`] = (map[`${r},${c}`] || 0) + 1
-//                 if(map[`${r},${c}`] > max){
-//                 max = map[`${r},${c}`]
-//                 count = 1
-//                 }else if(map[`${r},${c}`] === max){
-//                     count ++
-//                 }
-//             }
-//         }
-//     }
-
-
-//     for([row,col] of ops){
-//         addToMap(row ,col)
-//     }
-
-//     // let values = map.values()
-
-//     // max = Math.max(...values)
-    
-//     // values.find()
-
-//     return count
-    
-
-// };
 /**
  * @param {number} m
  * @param {number} n
@@ -49,10 +5,8 @@
  * @return {number}
  */
 var maxCount = function(m, n, ops) {
-    // If there are no operations, the entire grid has the same count
     if (!ops.length) return m * n;
 
-    // Find the minimum row and column bounds from the operations
     let minRow = m;
     let minCol = n;
 
@@ -61,6 +15,5 @@ var maxCount = function(m, n, ops) {
         minCol = Math.min(minCol, col);
     }
 
-    // The area of the subgrid with the maximum count
     return minRow * minCol;
 };
