efac5fb96badca0d33c667aad236d288d8e8700f
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-10-28 13:03:46 -0700

    Time: 0 ms (100%), Space: 49.2 MB (27.94%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -1,17 +1,36 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
 var merge = function(nums1, m, nums2, n) {
-    let i = m - 1;
-    let j = n - 1;
-    let k = m + n - 1;
-    
-    while (j >= 0) {
-        if (i >= 0 && nums1[i] > nums2[j]) {
-            nums1[k] = nums1[i];
-            k--;
-            i--;
-        } else {
-            nums1[k] = nums2[j];
-            k--;
-            j--;
+    if(!nums2.length) return nums1
+    // if(m === 1 && n === 1) return nums2
+    const binarySearch = (target, length) => {
+        let start = 0
+        let end = length - 1
+
+        while(start <= end){
+            const mid = Math.floor((start + end) / 2)
+            if(nums1[mid] === target){
+                return mid
+            }else if(nums1[mid] < target){
+                start = mid + 1
+            }else{
+                end = mid - 1
+            }
         }
+
+        return start
+    }
+
+
+    for(let i = 0; i < n; i++){
+        let idx = binarySearch(nums2[i], m)
+        nums1.pop()
+        nums1.splice(idx, 0, nums2[i])
+        m ++;
     }
 };
