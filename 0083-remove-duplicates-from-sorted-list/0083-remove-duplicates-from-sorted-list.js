f48dc1b49b8db9da90b35556e75b55298d78c1bd
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-10-30 22:59:01 -0700

    Time: 2 ms (9.98%), Space: 52.6 MB (47.96%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.js
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.js
@@ -11,20 +11,15 @@
  */
 var deleteDuplicates = function(head) {
     let seen = new Set()
+    let retList = new ListNode(0)
+    let retCurr = retList
 
     for(let current = head; current; current = current.next){
-        seen.add(current.val)
-    }
-
-    seen = Array.from(seen)
-    // seen.sort((a,b) => a - b)
-    let retList = new ListNode(0)
-
-    let current = retList
-
-    for(const num of seen){
-        current.next = new ListNode(num)
-        current = current.next
+        if(!seen.has(current.val)){
+            seen.add(current.val)
+            retCurr.next = new ListNode(current.val)
+            retCurr = retCurr.next
+        }
     }
 
     return retList.next
