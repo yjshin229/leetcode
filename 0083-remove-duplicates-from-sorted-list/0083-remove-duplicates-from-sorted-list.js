c0feef31abff6de644b6e2e0668e28962adc7ad7
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-10-30 22:56:36 -0700

    Time: 3 ms (7.94%), Space: 53.3 MB (5.39%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    let seen = new Set()
+
+    for(let current = head; current; current = current.next){
+        seen.add(current.val)
+    }
+
+    seen = Array.from(seen)
+    seen.sort((a,b) => a - b)
+    let retList = new ListNode(0)
+
+    let current = retList
+
+    for(const num of seen){
+        current.next = new ListNode(num)
+        current = current.next
+    }
+
+    return retList.next
+
+
+};
