87b7dfd92b07afd878c914362ac9dffa839c5491
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-11-03 11:24:37 +0700

    Time: 0 ms (100%), Space: 50.3 MB (34.52%) - LeetHub

--- 1391-counting-elements/1391-counting-elements.js
+++ 1391-counting-elements/1391-counting-elements.js
@@ -12,7 +12,7 @@ var countElements = function(arr) {
         numMap[num] = (numMap[num] || 0) + 1
         if(numMap[num - 1]){
             count += numMap[num - 1]
-             numMap[num -1] = 0
+            numMap[num -1] = 0
         }
     }
     return count
