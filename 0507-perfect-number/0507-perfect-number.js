91345bd84886ab12ee008ec4eb7b1fd463d02fad
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-11-06 21:57:00 -0800

    Time: 2154 ms (5.42%), Space: 49.2 MB (25%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.js
+++ 0507-perfect-number/0507-perfect-number.js
@@ -3,15 +3,12 @@
  * @return {boolean}
  */
 var checkPerfectNumber = function(num) {
-    let divisors = []
     let i = 1
     let sum = 0
     while(i < num){
         if (num % i === 0){
             sum += i
-            divisors.push(i)
         }
-
         i ++;
     }
 
