a2eef2100032aa237c7d561929498dc1df21f6cb
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-11-08 20:54:34 -0800

    Time: 58 ms (73.83%), Space: 51.5 MB (32.21%) - LeetHub

---
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} word
+ * @return {number}
+ */
+var possibleStringCount = function(word) {
+    let wordArr = word.split("")
+    let left = 0
+    let right = 1
+    let count = 1
+    let res = 0
+
+    while(right <= wordArr.length){
+        if(wordArr[left] === wordArr[right]){
+            count ++;
+            right ++;
+        }else{
+            if(right - left === 1){
+                left ++;
+                right ++;
+            }else{
+                res += count - 1
+                count = 1
+                left = right
+                right++;
+            }
+        }
+    }
+
+    return res === 0? 1 : res + 1
+
+};
