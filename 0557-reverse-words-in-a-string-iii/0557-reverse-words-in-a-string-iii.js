1abcd98e79846be30537314285123d4cd917ca8a
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-11-12 21:08:41 -0800

    Time: 14 ms (19.91%), Space: 56 MB (28.44%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    let sArr = s.split(" ")
+
+    const reverseWord = (word) => {
+        let ret = ""
+        for(let i = word.length -1; i >=0; i --){
+            ret += word.substring(i, i+1)
+        }
+        return ret
+    }
+
+
+    for(let i = 0; i <sArr.length; i++){
+        sArr[i] = reverseWord(sArr[i])
+    }
+
+    return sArr.join(" ")
+};
