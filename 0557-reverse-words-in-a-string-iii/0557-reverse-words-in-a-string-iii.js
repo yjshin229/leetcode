ad2dd8f985cd7bc90dfc942ef5b62d3820871a26
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2024-11-12 21:13:05 -0800

    Time: 12 ms (38.53%), Space: 56.1 MB (20.67%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
@@ -28,7 +28,7 @@ var reverseWords = function(s) {
 
 
     for(let i=0; i< sArr.length; i++){
-        sArr[i] = reverseWord(sArr[i])
+        sArr[i] = sArr[i].split("").reverse().join("")
     }
 
     return sArr.join(" ")
